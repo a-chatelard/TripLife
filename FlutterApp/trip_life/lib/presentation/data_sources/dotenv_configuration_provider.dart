12bfb5c6712c48a484ac55203ed4e463bf780fa7
Julien Poty <julienmpoty@yahoo.fr>
2023-04-08 20:34:57 +0200

    add configuration provider

---
+++ FlutterApp/trip_life/lib/presentation/data_sources/dotenv_configuration_provider.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:trip_life/infrastructure/abstract/abstract_configuration_provider.dart';
+
+class DotEnvConfigurationProvider implements AbstractConfigurationProvider {
+  DotEnvConfigurationProvider._();
+
+  static Future<DotEnvConfigurationProvider> create() async {
+    DotEnvConfigurationProvider configurationProvider =
+        DotEnvConfigurationProvider._();
+
+    await dotenv.load(fileName: ".env");
+
+    return configurationProvider;
+  }
+
+  static dynamic dotenvConfig;
+
+  @override
+  String getStringValue(String key) {
+    return dotenv.get(key);
+  }
+}
