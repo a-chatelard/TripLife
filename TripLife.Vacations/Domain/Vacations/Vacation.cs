17a178ee03648f88d284c96e3c71828b30957cc7
Antoine CHATELARD <achatelard@myges.fr>
2023-05-18 18:29:21 +0200

    setup editor config Vacation MS

---
+++ TripLife.Vacations/Domain/Vacations/Vacation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Vacations;
+
+public class Vacation
+{
+    public Guid Id { get; }
+}
