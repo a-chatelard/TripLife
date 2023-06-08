c7eb976b1147fe3d1174448af688a05016b9881d
Antoine CHATELARD <achatelard@myges.fr>
2023-06-08 10:58:02 +0200

    added shared setup to vacations MS

--- TripLife.Vacations/WebApi/Program.cs
+++ TripLife.Vacations/WebApi/Program.cs
@@ -1,8 +1,6 @@
 using Application;
 using Infrastructure.Extensions;
-using Microsoft.AspNetCore.Server.Kestrel.Core;
-using Microsoft.Extensions.Configuration;
-using Microsoft.OpenApi.Models;
+using TripLife.Foundation.WebApi.Extension;
 using WebApi.Extensions;
 using WebApi.Middlewares;
 
@@ -16,43 +14,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Vacation - WebApi", Version = "v1" });
-
-    var filePath = Path.Combine(AppContext.BaseDirectory, "WebApi.xml");
-    c.IncludeXmlComments(filePath);
-
-    /*c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
-                      Enter 'Bearer' [space] and then your token in the text input below.
-                      \r\n\r\nExample: 'Bearer 12345abcdef'",
-        Name = "Authorization",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer"
-    });
-
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
-      {
-        {
-          new OpenApiSecurityScheme
-          {
-            Reference = new OpenApiReference
-              {
-                Type = ReferenceType.SecurityScheme,
-                Id = "Bearer"
-              },
-              Scheme = "oauth2",
-              Name = "Bearer",
-              In = ParameterLocation.Header,
-
-            },
-            new List<string>()
-          }
-        });*/
-});
+builder.Services.AddSwagger(configuration);
 
 builder.Services.AddMediatR(options =>
 {
@@ -68,12 +30,11 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment() || app.Environment.IsEnvironment("Local"))
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.EnableSwagger();
 }
 
-//app.UseAuthentication();
-//app.UseAuthorization();
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.UseMiddleware<ExceptionMiddleware>();
 
