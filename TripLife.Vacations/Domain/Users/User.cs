c8995c604bf9bfa966cc3ce4a8634f8dad5ef005
Antoine CHATELARD <achatelard@myges.fr>
2023-07-02 20:09:52 +0200

    Fixed username column on EfCore migration

--- TripLife.Vacations/Domain/Users/User.cs
+++ TripLife.Vacations/Domain/Users/User.cs
@@ -5,7 +5,7 @@ namespace Domain.Users;
 public class User
 {
     public Guid Id { get; }
-    public string? Username { get; }
+    public string? Username { get; private set; }
 
     internal User() { }
 
---
+++ TripLife.Vacations/Infrastructure/Migrations/20230702180913_AddedUsername.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedUsername : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Username",
+                table: "Users",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Username",
+                table: "Users");
+        }
+    }
+}
